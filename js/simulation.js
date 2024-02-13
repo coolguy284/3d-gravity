74ca0a27b3b6a60c1a34ee8ba66170a37acbac1b
coolguy284 <coolguy284v64@gmail.com>
2024-02-12 22:16:54 -0500

    partially implement 2d simulation
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- js/simulation.js
+++ js/simulation.js
@@ -6,7 +6,62 @@ function resetScenario() {
 function simulate(timeStep) {
   switch (mode) {
     case '2d':
-      // unimplemented for now
+      // apply gravity
+      for (let i = 0; i < particles.length; i++) {
+        for (let j = i + 1; j < particles.length; j++) {
+          // calculate double-sided interaction simultaneously for both particles
+          let particle1 = particles[i];
+          let particle2 = particles[j];
+          
+          let dist = Math.hypot(
+            particle2[3] - particle1[3],
+            particle2[4] - particle1[4]
+          );
+          
+          let forceStrength = GRAV_STRENGTH * particle1[0] * particle2[0];
+          
+          if (dist == 0) {
+            forceStrength /= GRAV_DIST_LOWER_LIMIT * GRAV_DIST_LOWER_LIMIT;
+          } else if (dist < GRAV_DIST_LOWER_LIMIT) {
+            forceStrength /= GRAV_DIST_LOWER_LIMIT * GRAV_DIST_LOWER_LIMIT;
+          } else {
+            forceStrength /= dist * dist;
+          }
+          
+          let forceNormX, forceNormY;
+          
+          if (dist == 0) {
+            let angle = 2 * Math.PI * rng();
+            
+            forceNormX = Math.cos(angle);
+            forceNormY = Math.sin(angle);
+          } else {
+            forceNormX = (particle2[3] - particle1[3]) / dist;
+            forceNormY = (particle2[4] - particle1[4]) / dist;
+          }
+          
+          let forceX = forceNormX * forceStrength;
+          let forceY = forceNormY * forceStrength;
+          
+          let accel1X = forceX / particle1[0];
+          let accel1Y = forceY / particle1[0];
+          
+          let accel2X = -forceX / particle2[0];
+          let accel2Y = -forceY / particle2[0];
+          
+          particle1[5] += accel1X * timeStep;
+          particle1[6] += accel1Y * timeStep;
+          
+          particle2[5] += accel2X * timeStep;
+          particle2[6] += accel2Y * timeStep;
+        }
+      }
+      
+      // apply velocity
+      for (let particle of particles) {
+        particle[3] += particle[5] * timeStep;
+        particle[4] += particle[6] * timeStep;
+      }
       break;
     
     case '3d':
